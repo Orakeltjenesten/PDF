0eef632d7dc9933cab63893421097e1ec52ce41d
hansmariusoveras <47254808+hansmariusoveras@users.noreply.github.com>
2021-06-15 13:12:05 +0200

    added pdf test files and reorder pdfs

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,10 +4,76 @@ import styles from '../styles/Home.module.css'
 import Link from 'next/link'
 import { SyntheticEvent } from 'react'
 import {PDFDocument, StandardFonts} from 'pdf-lib'
-let currentFile : File;
+import { fileSave } from 'browser-fs-access'
+let files : File[] = [];
+let entryText : string;
+
+function moveOneUp(name : string) {
+  for (let i=0; i<files.length; i++) {
+    if (name.includes(files[i].name)) {
+      if (i == 0) {
+        let f : File = files.splice(0,1)[0]
+        files.push(f);
+      } else {
+        files.splice(i-1, 0, files.splice(i, 1)[0]);
+      }
+      break;
+    }
+  }
+  renderEntries();
+}
+
+function deleteEntry(name: string) {
+  for (let i=0; i<files.length; i++) {
+    if (name.includes(files[i].name)) {
+      files.splice(i, 1);
+      break;
+    }
+  }
+  renderEntries();
+}
+
+function addEntry(text : string) {
+  var div = document.createElement("div");
+
+  var upButton = document.createElement("button");
+  upButton.onclick = (e) => moveOneUp(text);
+  upButton.innerText = "^"
+
+  var delButton = document.createElement("button");
+  delButton.onclick = (e) => deleteEntry(text);
+  delButton.innerText = "X";
+
+  var textDiv = document.createElement("div");
+  textDiv.innerText = text;
+
+  var buttons = document.createElement("div");
+  textDiv.style.flex = "5 1 0";
+  buttons.style.flex = "1 1 0"
+  textDiv.style.whiteSpace = "nowrap"
+  textDiv.style.overflow = "hidden";
+  textDiv.style.textOverflow = "ellipsis"
+  
+  buttons.style.justifyContent = "right";
+  buttons.appendChild(upButton);
+  buttons.appendChild(delButton);
+
+  div.appendChild(textDiv);
+  div.appendChild(buttons);
+
+  document.getElementById("entries")!.appendChild(div);
+}
 
 
-function UploadButton(text : String) {
+function renderEntries() {
+  document.getElementById("entries")!.innerHTML = "";
+  for (let i=0;i<files.length; i++) {
+    addEntry(i.toString() + ". " + files[i].name);
+  }
+}
+
+
+function UploadButton(text : string) {
 
   function triggerUpload(e: SyntheticEvent){
     e.preventDefault();
@@ -16,10 +82,10 @@ function UploadButton(text : String) {
 
   }
 
-  function handleFiles(files: FileList) {
-    let firstFile : File | null = files.item(0);
-    if (firstFile != null) {
-      currentFile = firstFile;
+  function handleFiles(handledFiles: FileList | null) {
+    if (handledFiles != null) {
+      files = Array.from(handledFiles);
+      renderEntries();
     }
   }
 
@@ -27,36 +93,21 @@ function UploadButton(text : String) {
     <div>
     <input className={styles.hidden} onChange={(e) => handleFiles(e.target.files)} type="file" id="file1" multiple/>
 
-    <input className={styles.upload} type="button" value="Upload" onClick={triggerUpload} />
+    <input className={styles.upload} type="button" value={text} onClick={triggerUpload} />
     </div>
   )
 }
 
-function SavePDFButton(text: String) {
+function SavePDFButton(text: string) {
 
-  async function savePDF(e: SyntheticEvent) {
-    e.preventDefault();
-    /*try {
+  async function assemblePDF(pdfs : PDFDocument[]) {
     
-    const pdfDoc = await PDFDocument.create();
-    const font = await pdfDoc.embedFont(StandardFonts.Helvetica);
-    const first = await PDFDocument.load(await file1.arrayBuffer());
-    const second = await PDFDocument.load(await file2.arrayBuffer());
-
-    const copiedFirst = await pdfDoc.copyPages(first, first.getPageIndices())
-    const copiedSecond = await pdfDoc.copyPages(second, second.getPageIndices());
-
-    copiedFirst.forEach((page) => pdfDoc.addPage(page));
-    copiedSecond.forEach((page) => pdfDoc.addPage(page));
-
-    const writable = (await saveLocation())
-    writable.write(await pdfDoc.save());
-
-    writable.close();
-
-    } catch (error) {
-      console.log(error);
-    }*/
+    const merged = await PDFDocument.create();
+    pdfs.forEach(async (pdf) => (await merged.copyPages(pdf, pdf.getPageIndices()))
+    .forEach((page) => merged.addPage(page))
+    );
+    
+    return merged.save({addDefaultPage: false});
   }
 
   async function getDownload(e : SyntheticEvent) {
@@ -65,16 +116,20 @@ function SavePDFButton(text: String) {
     
     let url: string;
     let name: string;
+    renderEntries();
 
-    if (currentFile == null) {
-      // Create a test file
-      let blob = new Blob(["You had not uploaded a file, so this empty one was returned."]);
-      url = window.URL.createObjectURL(blob);
-      name = "empty.txt"
+    if (files == null || files.length == 0) {
+      alert("No files were uploaded.")
+      return
     } else {
-      // Embed the current file in a URL
-      url = window.URL.createObjectURL(currentFile);
-      name = "your_file_as_text.txt"
+      let pdfList : PDFDocument[] = [];
+      for (let i=0; i<files.length; i++) {
+          let pdf = await PDFDocument.load(await files[i].arrayBuffer());
+          pdfList.push(pdf);
+      }
+      const merged : Uint8Array = await assemblePDF(pdfList);
+      url = window.URL.createObjectURL(new Blob([merged]));
+      name = "merged.pdf"
     }
 
     // Create a ghost 'a'-element
@@ -83,20 +138,18 @@ function SavePDFButton(text: String) {
     a.setAttribute("style", "display: none;");
 
     // Set its download and href attributes accordingly to filename and URL of file
-    a.download = name;
-    a.href = url;
+    a.download = name!;
+    a.href = url!;
     a.click();
 
     a.remove();
+    renderEntries();
   }
 
 
   return (
-    <input className={styles.upload} type="button" value="Save" onClick={getDownload} />
+    <input className={styles.upload} type="button" value={text} onClick={getDownload} />
   )
-
-
-  
 }
 
 
@@ -104,19 +157,24 @@ export default function Home() {
   return (
     <body>
       <Head>
-        <title>Merge 2 PDFs</title>
+        <title>PDF MERGER</title>
         <meta name="Test Project" content="Test creation" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main className={styles.main}>
         <h1>
-          Merge two PDFs.
+          PDF Merger
         </h1>
 
 
-        {UploadButton("Upload PDF1")}
-        {SavePDFButton("Save")}
+        {UploadButton("Upload PDFs")}
+
+        <div id="entries" className={styles.entries}>
+          
+        </div>
+
+        {SavePDFButton("Merge")}
       </main>
 
       
