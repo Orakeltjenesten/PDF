132f380346391eec61456db0fd60d636a34d49ac
hansmariusoveras <47254808+hansmariusoveras@users.noreply.github.com>
2021-08-06 13:47:58 +0200

    started making split cards

--- components/SplitGrid.tsx
+++ components/SplitGrid.tsx
@@ -6,6 +6,8 @@ import { makeStyles, createStyles}  from '@material-ui/styles/';
 import { Theme } from "@material-ui/core/styles/";
 import MaterialPaper from '@material-ui/core/Paper';
 import { Card, FormLabel, Grid } from '@material-ui/core';
+import PDFSplitDisplayEntry from './PDFSplitDisplayEntry';
+import { UploadedFile } from '../hooks/UploadedFile';
 
 const useStyles = makeStyles((theme: Theme) => 
     createStyles({
@@ -19,20 +21,20 @@ const useStyles = makeStyles((theme: Theme) =>
 }));
 
 export type SplitGridProps = {
-  files: File[]
+  uploadedFiles: UploadedFile[]
   className?: string;
 };
 
-const SplitGrid = ({ files, className }: SplitGridProps) => {
+const SplitGrid = ({ uploadedFiles, className }: SplitGridProps) => {
   const classes = useStyles();
   return (
     <Grid container className={classnames(classes.root, className)} spacing={2}>
       <Grid item xs={12}>
         <Grid container justifyContent="center" spacing={2}>
-          {files.map((file) => (
-            <Grid key={file.name} item>
-              <Card className={classes.paper}>{file.name}</Card>
-            </Grid>
+          {uploadedFiles.map((uploadedFile) => (
+            <Card className={classes.paper}>
+              <PDFSplitDisplayEntry uploadedFile={uploadedFile} index={0}></PDFSplitDisplayEntry>
+            </Card>
           ))}
         </Grid>
       </Grid>
