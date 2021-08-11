790c8704cefa96cd79121a401811a875d69287ca
hansmariusoveras <47254808+hansmariusoveras@users.noreply.github.com>
2021-08-11 13:32:28 +0200

    css

--- components/globals/Layout.tsx
+++ components/globals/Layout.tsx
@@ -6,23 +6,42 @@ import { makeStyles, createStyles}  from '@material-ui/styles';
 import useMediaQuery from '@material-ui/core/useMediaQuery';
 import { Theme } from "@material-ui/core/styles";
 import { ReactNode } from "react";
+import useTranslation from "next-translate/useTranslation";
+import { autocompleteClasses } from "@material-ui/core";
 
 const useStyles = makeStyles((theme: Theme) => 
     createStyles({
       wrapper: {
+        display: 'flex',
+        flexDirection: 'column',
         background: theme.palette.background.default,
-        minHeight: '100vh',
+        height: '100vh',
       },
       offset: theme.mixins.toolbar,
+      
+      footer: {
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        padding: '4px',
+        marginTop: 'auto',
+      }
+      
     })
+    
 );
 export default function Layout({children} : {children: ReactNode}) {
     const classes = useStyles();
+    const {t} = useTranslation("common");
   return (
     <div className={classes.wrapper}>
       <Topbar variant='dynamic'/>
       <div className={classes.offset}></div>
       <main>{children}</main>
+
+      <footer className={classes.footer}>
+        {t("with_love")}
+      </footer>
     </div>
   )
 }
