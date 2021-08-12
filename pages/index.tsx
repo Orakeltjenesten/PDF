3a2864f8df0733a082ba16404d3bd21cf965950e
Simon Jensen <simon.jensen.lg@gmail.com>
2021-08-12 09:19:30 +0200

    Visual improvements

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,7 @@ import Head from 'next/head'
 import React from 'react'
 
 // Material UI Components
-import { makeStyles, createStyles}  from '@material-ui/styles/';
+import { makeStyles, createStyles } from '@material-ui/styles/';
 import { Theme } from "@material-ui/core/styles";
 import { Button, Typography } from '@material-ui/core'
 import Logo from '../components/globals/Logo'
@@ -10,36 +10,20 @@ import Link from 'next/link'
 import useTranslation from 'next-translate/useTranslation';
 
 
-const useStyles = makeStyles((theme: Theme) => 
+const useStyles = makeStyles((theme: Theme) =>
     createStyles({
-        header: {
-            marginTop: theme.spacing(1),
-        },
-        cover: {
+        root: {
+            flex: 1,
             position: 'relative',
             color: theme.palette.primary.light,
-            height: '100vh',
             width: '100%',
             display: 'flex',
             flexDirection: 'column',
             justifyContent: 'center',
             alignItems: 'center',
         },
-        img: {
-            //background: `${theme.palette.colors.gradient}, url(${image}) center center/cover no-repeat scroll`,
-            objectFit: 'contain',
-            boxShadow: 'inset 0 0 0 1000px rgba(0, 0, 0, 0.2)',
-            position: 'absolute',
-            top: 0,
-            bottom: 0,
-            left: 0,
-            right: 0,
-            filter: 'blur(1px)',
-            zIndex: -1,
-        },
-        activityContainer: {
-            textAlign: 'center',
-            paddingTop: theme.spacing(2),
+        header: {
+            marginTop: theme.spacing(1),
         },
         btnGroup: {
             display: 'grid',
@@ -57,18 +41,8 @@ const useStyles = makeStyles((theme: Theme) =>
             maxHeight: 200,
             marginBottom: theme.spacing(2),
         },
-        logo: {
-            minWidth: '250px',
-            width: '46%',
-            maxWidth: '100%',
-            height: 'auto',
-            margin: theme.spacing(5, 'auto'),
-            [theme.breakpoints.down('md')]: {
-                minWidth: '200px',
-            },
-        },
     }
-));
+    ));
 
 
 export default function Home() {
@@ -78,16 +52,15 @@ export default function Home() {
         <>
             <Head>
                 <title>{t("index_title")}</title>
-                <meta name={t("meta_name")} content={t("upload")}/>
-                <link rel="icon" href="/favicon.ico"/>
+                <meta name={t("meta_name")} content={t("upload")} />
+                <link rel="icon" href="/favicon.ico" />
             </Head>
-            <div className={classes.cover}>
-                <div className={classes.img} />
+            <main className={classes.root}>
                 <div className={classes.logoWrapper}>
-                    <Logo darkColor='white' lightColor='black'/>
+                    <Logo darkColor='white' lightColor='black' />
                 </div>
                 <Typography align='center' color='inherit' variant='h3'>
-                {t("welcome_message")}
+                    {t("welcome_message")}
                 </Typography>
                 <div className={classes.btnGroup}>
                     <Link href="/upload">
@@ -96,7 +69,7 @@ export default function Home() {
                         </Button>
                     </Link>
                 </div>
-            </div>
+            </main>
         </>
     )
 }
